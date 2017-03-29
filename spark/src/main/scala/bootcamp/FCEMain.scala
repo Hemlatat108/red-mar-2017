07261c1abe16c6845b10b2881f48f1401877b288
cfusi <claudio.fusi@cloudera.com>
2017-03-29 15:48:39 -0700

    Sample spark project

---
+++ spark/src/main/scala/bootcamp/FCEMain.scala
@@ -0,0 +1,19 @@
+package bootcamp
+
+import java.util.Properties
+
+import kafka.serializer.StringDecoder
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.functions._
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.util.Try
+
+object KafkaSparkDatalake {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
